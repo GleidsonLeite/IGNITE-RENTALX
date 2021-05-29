fab76ff325435d00598391b56f571f8f103388b9
Gleidson Leite <gledson.leytte@hotmail.com>
2021-05-29 14:10:42 -0300

    Singleton Pattern

--- src/modules/cars/repositories/CategoriesRepository.ts
+++ src/modules/cars/repositories/CategoriesRepository.ts
@@ -4,10 +4,22 @@ import {
   ICreateCategoryDTO,
 } from './ICategoriesRepository';
 
+// singleton
+
 class CategoriesRepository implements ICategoriesRepository {
   private categories: Category[];
-  constructor() {
+
+  private static INSTANCE: CategoriesRepository;
+
+  private constructor() {
     this.categories = [];
+  }
+
+  public static getInstance(): CategoriesRepository {
+    if (!CategoriesRepository.INSTANCE) {
+      CategoriesRepository.INSTANCE = new CategoriesRepository();
+    }
+    return CategoriesRepository.INSTANCE;
   }
 
   create({ name, description }: ICreateCategoryDTO): void {
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,19 +1,16 @@
 import { Request, Response, Router } from 'express';
 
-import { CategoriesRepository } from '../modules/cars/repositories/CategoriesRepository';
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
+import { listCategoriesController } from '../modules/cars/useCases/ListCategories';
 
 const categoriesRouter = Router();
-
-const categoriesRepository = new CategoriesRepository();
 
 categoriesRouter.post('/', (request: Request, response: Response) => {
   return createCategoryController.handle(request, response);
 });
 
 categoriesRouter.get('/', (request: Request, response: Response) => {
-  const categories = categoriesRepository.list();
-  return response.status(200).json({ categories });
+  return listCategoriesController.handle(request, response);
 });
 
 export { categoriesRouter };
