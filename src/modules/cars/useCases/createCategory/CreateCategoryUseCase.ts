3e37cc833b0d7fe7d21748d4062499c99cbdcf00
Gleidson Leite <gledson.leytte@hotmail.com>
2021-05-29 13:52:26 -0300

    Starting controllers and useCases

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -1,15 +1,16 @@
-import { ICategoriesRepository } from '../repositories/ICategoriesRepository';
+import { ICategoriesRepository } from '../../repositories/ICategoriesRepository';
 
 interface IRequest {
   name: string;
   description: string;
 }
 
-class CreateCategoryService {
+class CreateCategoryUseCase {
   constructor(private categoriesRepository: ICategoriesRepository) {}
 
   execute({ name, description }: IRequest): void {
     const categoryAlreadyExists = this.categoriesRepository.findByName(name);
+
     if (categoryAlreadyExists) {
       throw new Error('Category already exists!');
     }
@@ -21,4 +22,4 @@ class CreateCategoryService {
   }
 }
 
-export { CreateCategoryService };
+export { CreateCategoryUseCase };
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,21 +1,14 @@
 import { Request, Response, Router } from 'express';
 
 import { CategoriesRepository } from '../modules/cars/repositories/CategoriesRepository';
-import { CreateCategoryService } from '../modules/cars/services/CreateCategoryService';
+import { createCategoryController } from '../modules/cars/useCases/createCategory';
 
 const categoriesRouter = Router();
 
 const categoriesRepository = new CategoriesRepository();
 
 categoriesRouter.post('/', (request: Request, response: Response) => {
-  const { name, description } = request.body;
-
-  const createCategoryService = new CreateCategoryService(categoriesRepository);
-  createCategoryService.execute({
-    name,
-    description,
-  });
-  return response.status(201).send();
+  return createCategoryController.handle(request, response);
 });
 
 categoriesRouter.get('/', (request: Request, response: Response) => {
