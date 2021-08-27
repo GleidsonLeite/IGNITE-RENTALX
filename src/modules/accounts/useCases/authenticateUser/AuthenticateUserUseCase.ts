88fc6aa030666eeea893d3ca08c58334ed0594c8
Gleidson Leite <gledson.leytte@hotmail.com>
2021-08-27 00:09:32 -0300

    User authentication

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,58 @@
+import { compare } from 'bcrypt';
+import { sign } from 'jsonwebtoken';
+import { inject, injectable } from 'tsyringe';
+
+import { IUsersRepository } from '../../repositories/IUsersRepository';
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IUser {
+  name: string;
+  email: string;
+}
+
+interface IResponse {
+  user: IUser;
+  token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+  ) {}
+
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.findByEmail(email);
+    if (!user) {
+      throw new Error('Email or password incorrect');
+    }
+
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new Error('Email or password incorrect');
+    }
+
+    const token = sign({}, 'c2801a8115696493b71426bbe0ffd601', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    const tokenInfo: IResponse = {
+      token,
+      user: {
+        email: user.email,
+        name: user.name,
+      },
+    };
+
+    return tokenInfo;
+  }
+}
+
+export { AuthenticateUserUseCase };
