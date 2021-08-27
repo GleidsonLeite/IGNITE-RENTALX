d0e7bef3ce51d23d166e189ba361d4c27a496b64
Gleidson Leite <gledson.leytte@hotmail.com>
2021-08-27 00:34:38 -0300

    Using middleware

--- src/modules/accounts/repositories/implementations/UsersRepository.ts
+++ src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -10,6 +10,12 @@ class UsersRepository implements IUsersRepository {
   constructor() {
     this.repository = getRepository(User);
   }
+
+  async findById(id: string): Promise<User> {
+    const user = await this.repository.findOne(id);
+    return user;
+  }
+
   async findByEmail(email: string): Promise<User> {
     const user = await this.repository.findOne({
       email,
