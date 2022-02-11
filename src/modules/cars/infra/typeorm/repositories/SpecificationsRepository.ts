263b0f0586a0b661d0d7203841f286511fb10b6e
Gleidson Leite <gledson.leytte@hotmail.com>
2022-02-11 12:09:53 -0300

    Refactoring Application

--- src/modules/cars/infra/typeorm/repositories/SpecificationsRepository.ts
+++ src/modules/cars/infra/typeorm/repositories/SpecificationsRepository.ts
@@ -1,10 +1,11 @@
 import { getRepository, Repository } from 'typeorm';
 
-import { Specification } from '../../entities/Specification';
 import {
-  ISpecificationRepository,
   ICreateSpecificationDTO,
-} from '../ISpecificationRepository';
+  ISpecificationRepository,
+} from '@modules/cars/repositories/ISpecificationRepository';
+
+import { Specification } from '../entities/Specification';
 
 class SpecificationsRepository implements ISpecificationRepository {
   private repository: Repository<Specification>;
