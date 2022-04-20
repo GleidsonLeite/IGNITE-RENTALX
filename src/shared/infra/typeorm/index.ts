85e48509c705898cf080ee7a6ce6ded7f85a70c6
Gleidson Leite <gledson.leytte@hotmail.com>
2022-04-19 23:48:46 -0300

    Creating admin seed

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,13 +1,8 @@
-import { createConnection, getConnectionOptions } from 'typeorm';
+import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 
-interface IOptions {
-  host: string;
+export default async (host = "database_ignite"): Promise<Connection> => {
+  const defaultOptions = await getConnectionOptions();
+  return createConnection(
+    Object.assign(defaultOptions, { host })
+  )
 }
-
-getConnectionOptions().then(options => {
-  const newOptions = options as IOptions;
-  newOptions.host = 'database_ignite';
-  createConnection({
-    ...options,
-  });
-});
