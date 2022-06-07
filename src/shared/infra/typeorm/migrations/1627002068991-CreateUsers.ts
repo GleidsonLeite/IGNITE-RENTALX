f01b9dfdda0f6d1467247f29bb27aa6738b71902
Gleidson Leite <gledson.leytte@hotmail.com>
2022-06-06 21:28:28 -0300

    migration created

--- src/shared/infra/typeorm/migrations/1627002068991-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1627002068991-CreateUsers.ts
@@ -9,6 +9,7 @@ export class CreateUsers1627002068991 implements MigrationInterface {
           {
             name: 'id',
             type: 'uuid',
+            isPrimary: true,
           },
           {
             name: 'name',
