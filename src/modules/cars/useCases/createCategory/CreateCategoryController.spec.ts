2b62f479ed2feffa469059e8ce25932cec4a4b0e
Gleidson Leite <gledson.leytte@hotmail.com>
2023-05-04 23:59:17 -0300

    configuring integration tests

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -0,0 +1,13 @@
+import request from 'supertest';
+
+import { app } from '@shared/infra/http/app';
+
+describe('Create Category Controller', () => {
+  it('should be able to create a new category', async () => {
+    const response = await request(app).post('/categories').send({
+      name: 'Category Supertest',
+      description: 'Category Supertest',
+    });
+    expect(response.status).toBe(201);
+  });
+});
--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,8 +1,14 @@
 import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 
-export default async (host = "database_ignite"): Promise<Connection> => {
+export default async (host = 'database_ignite'): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
   return createConnection(
-    Object.assign(defaultOptions, { host })
-  )
-}
+    Object.assign(defaultOptions, {
+      host: process.env.NODE_ENV === 'test' ? 'localhost' : host,
+      database:
+        process.env.NODE_ENV === 'test'
+          ? 'rentx_test'
+          : defaultOptions.database,
+    }),
+  );
+};
